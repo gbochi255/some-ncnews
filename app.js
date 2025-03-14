14dc8d450fcfde850337e7438b6d534072218591
gbochi255 <gbochi255@hotmail.com>
2025-03-14 15:37:32 +0000

    connection

--- app.js
+++ app.js
@@ -22,16 +22,21 @@ app.get("/api", (req, res) => {
 })
 app.get("/api/users", getUsers)
 
+
 app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
+
 app.get("/api/topics", getTopics);
 
 //app.get("/api/articles", getArticles);
 //app.get("/api/articles", getArticlesSort);
 
 app.get("/api/articles/:article_id", getArticleById);
+
 app.patch("/api/articles/:article_id", patchArticleById)
 
+
 app.post("/api/articles/:article_id/comments", postCommentByArticleId);
+
 app.delete("/api/comments/:comment_id", removeCommentById);
 
 
--- db/connection.js
+++ db/connection.js
@@ -3,36 +3,22 @@ const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || 'development'
 
 require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+}
 
 const config = ENV === "production"
 
-
 ? {
+
     connectionString: process.env.DATABASE_URL,
+    ssl: {rejectUnauthorized: false},
   }
 : {
     database: process.env.PGDATABASE,
   };
-
-if (ENV === "production") {
-  config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
-}
-
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-    throw new Error("PGDATABASE or DATABASE_URL not set");
-  }
-  
-
-
-
-
-
-if (!process.env.PGDATABASE) {
-    throw new Error("No PGDATABASE configured")
-} else { 
-    console.log(`Connected to ${process.env.PGDATABASE}`)
-}
-
 const db = new Pool(config);
+
+
+
 module.exports = new Pool(config);
